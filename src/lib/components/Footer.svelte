06c2f02bc11277f55c0b9c5e61c4309972d5754f
Kilimanjjjaro <hola@kilimanjjjaro.com>
2023-05-05 13:02:39 -0300

    Fix position and add github logo in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,20 @@
-<footer class="absolute bottom-0 left-0 flex justify-between w-full p-8 text-sm">
+<script>
+	import GithubLogo from '$lib/components/svg/GithubLogo.svelte'
+</script>
+
+<footer class="fixed bottom-0 left-0 flex justify-between w-full p-8 text-sm">
 	<p class="text-gray-600">© Kilimanjjjaro</p>
-	<a
-		class="text-white transition-colors duration-300 ease-in-out xl:hover:text-primary"
-		href="https://www.apple.com/stevejobs/"
-		target="_blank"
-		rel="noopener noreferrer">Steve</a
-	>
+	<div class="flex items-center gap-6">
+		<a
+			class="text-white transition-colors duration-300 ease-in-out xl:hover:text-primary"
+			href="https://www.apple.com/stevejobs/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Steve
+		</a>
+		<a href="https://github.com/kilimanjjjaro" target="_blank" rel="noopener noreferrer">
+			<GithubLogo />
+		</a>
+	</div>
 </footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
 	import Logo from '$lib/components/svg/KilimanjjjaroLogo.svelte'
 </script>
 
-<header class="absolute top-0 left-0 flex p-8">
+<header class="fixed top-0 left-0 flex p-8">
 	<a href="https://kilimanjjjaro.com/">
 		<Logo />
 	</a>
